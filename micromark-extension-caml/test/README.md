41e9c767890a25ebb4680f234f810899e714139d
manunamz <manunamz@pm.me>
2023-10-12 16:11:45 -0400

    ref: leave out micromark-level tests for now.

---
+++ micromark-extension-caml/test/README.md
@@ -0,0 +1,1 @@
+note: there is no generated html at the micromark level since we rely on mdast utils to build the attrbox later in the process.
--- micromark-extension-caml/test/runner.spec.ts
+++ micromark-extension-caml/test/runner.spec.ts
@@ -1,43 +1,45 @@
-import assert from 'node:assert/strict';
+// todo: html data tests
 
-// import type { CamlValData } from 'caml-mkdn';
+// import assert from 'node:assert/strict';
 
-import { micromark } from 'micromark';
+// // import type { CamlValData } from 'caml-mkdn';
 
-import type { CamlOptions } from '../src/util/types';
-import { htmlCaml, syntaxCaml } from '../src';
+// import { micromark } from 'micromark';
 
-import type { CamlTestCase } from 'caml-spec';
-import { camlCases } from 'caml-spec';
+// import type { CamlOptions } from '../src/util/types';
+// import { htmlCaml, syntaxCaml } from '../src';
+
+// import type { CamlTestCase } from 'caml-spec';
+// import { camlCases } from 'caml-spec';
 
 
-let mockOpts: Partial<CamlOptions>;
+// let mockOpts: Partial<CamlOptions>;
 
-function run(contextMsg: string, tests: CamlTestCase[]): void {
-  context(contextMsg, () => {
-    let i: number = 0;
-    for(const test of tests) {
-      const desc: string = `[${('00' + (++i)).slice(-3)}] ` + (test.descr || '');
-      it(desc, () => {
-        const mkdn: string = test.mkdn;
-        const expdHTML: string = test.html;
-        // const expdData: CamlValData = test.data;
-        const actlHTML: string = micromark(mkdn, {
-          extensions: [syntaxCaml()],
-          htmlExtensions: [htmlCaml(mockOpts)],
-        });
-        // const actlData: CamlValData = {};
-        assert.strictEqual(actlHTML.replace(/\n/g, ''), expdHTML.replace(/\n/g, ''));
-        // assert.deepStrictEqual(actlData, expdData);
-      });
-    }
-  });
-}
+// function run(contextMsg: string, tests: CamlTestCase[]): void {
+//   context(contextMsg, () => {
+//     let i: number = 0;
+//     for(const test of tests) {
+//       const desc: string = `[${('00' + (++i)).slice(-3)}] ` + (test.descr || '');
+//       it(desc, () => {
+//         const mkdn: string = test.mkdn;
+//         const expdHTML: string = test.html;
+//         // const expdData: CamlValData = test.data;
+//         const actlHTML: string = micromark(mkdn, {
+//           extensions: [syntaxCaml()],
+//           htmlExtensions: [htmlCaml(mockOpts)],
+//         });
+//         // const actlData: CamlValData = {};
+//         assert.strictEqual(actlHTML.replace(/\n/g, ''), expdHTML.replace(/\n/g, ''));
+//         // assert.deepStrictEqual(actlData, expdData);
+//       });
+//     }
+//   });
+// }
 
-describe('micromark-extension-caml', () => {
+// describe('micromark-extension-caml', () => {
 
-  // go
-  run('mkdn -> html', camlCases);
-  it.skip('html -> mkdn; precision newlines');
+//   // go
+//   run('mkdn -> html', camlCases);
+//   it.skip('html -> mkdn; precision newlines');
 
-});
+// });
